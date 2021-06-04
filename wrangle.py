6738f98c9056ae635e4dfacf53fd97589ee72069
Carl Griffin <carl.griffin@yahoo.com>
2021-06-04 18:43:48 -0500

    latest changes

--- wrangle.py
+++ wrangle.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import sklearn.preprocessing
 import os
 
 # Enables access to my env.py file in order to use sensitive info to access Codeup DB
@@ -79,4 +80,14 @@ def wrangle_telco():
     df = prep_telco_data(df)
     return df
 
+def Min_Max_Scaler(X_train, X_validate, X_test):
+    '''
+    Takes in X_train, X_validate and X_test dfs with numeric values only
+    Returns scaler, X_train_scaled, X_validate_scaled, X_test_scaled dfs 
+    '''
+    scaler = sklearn.preprocessing.MinMaxScaler().fit(X_train)
+    X_train_scaled = pd.DataFrame(scaler.transform(X_train), index = X_train.index, columns = X_train.columns)
+    X_validate_scaled = pd.DataFrame(scaler.transform(X_validate), index = X_validate.index, columns = X_validate.columns)
+    X_test_scaled = pd.DataFrame(scaler.transform(X_test), index = X_test.index, columns = X_test.columns)
     
+    return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
